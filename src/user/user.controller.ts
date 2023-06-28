f26d750e10113678dbd5f940e62c00327a344617
David Macdonald <ardothe1@yahoo.com>
2023-06-28 18:56:23 +0500

    Minor fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -17,10 +17,14 @@ import { InitializeUserDto, UpdatePublicProfileDto, UpdateSettingsDto } from "./
 import { GetUser } from "src/auth/decorators/getUser.decorator";
 import { RequireAuth } from "src/auth/decorators/auth.decorator";
 import { IUserWithPublicProfile } from "src/utilities/types/auth";
+import { revalidatePage } from "src/utilities/utils/auth";
+import { PrsimaService } from "src/prisma/prisma.service";
+import { Role } from "@prisma/client";
+import { getOwnerMetoken } from "graphql/Queries/GetOwnerMetoken";
 
 @Controller("user")
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private prismaService: PrsimaService) {}
 
   @Get("/profile")
   @RequireAuth()
@@ -73,8 +77,135 @@ export class UserController {
   }
 
   @Post("/test")
-  async test() {
-    return "Test endpoint";
+  async test(@Body() testInput: { address: string; key: string; value: string }) {
+    const users: any = await this.prismaService.users.findRaw();
+    // const APIURL = "https://api.studio.thegraph.com/query/35797/metokens-core-goerli/v0.2.0";
+
+    const newUsers = await Promise.all(
+      users.map(async (user: any) => {
+        const metokensArray = await getOwnerMetoken(user.address);
+        const ownerMetokenAddress = metokensArray.length > 0 ? metokensArray[0].id : "";
+        const ownerMetokenName = metokensArray.length > 0 ? metokensArray[0].name : "";
+        const ownerMetokenSymbol = metokensArray.length > 0 ? metokensArray[0].symbol : "";
+        const item = {
+          address: user.address || "",
+          email: user.email || "",
+          notifications: getNotifications(user),
+          oneTimeKey: generateOneTimeKey(),
+          isPriorityUser: !!user.isPriorityuser,
+          attributes: {},
+          priorityRank: !!user.isPriorityuser && !!user.rank ? user.rank : -1,
+          gateToken: user.gateToken,
+          role: user.isAdmin ? Role.ADMIN : ownerMetokenAddress === "" ? Role.GUEST : Role.USER,
+          publicProfile: {
+            address: user.address || "",
+            ens: user.ens || "",
+            username: user.name || "",
+            description: user.description || "",
+            pfp: getProfileImage(user),
+            cover: getCoverImage(user),
+            metoken: {
+              address: ownerMetokenAddress,
+              name: ownerMetokenName,
+              symbol: ownerMetokenSymbol,
+            },
+          },
+        };
+        return await this.prismaService.user.create({
+          data: {
+            address: item.address,
+            email: item.email,
+            notifications: item.notifications,
+            oneTimeKey: item.oneTimeKey,
+            role: item.role,
+            isPriorityUser: item.isPriorityUser,
+            priorityRank: item.priorityRank,
+            publicProfile: {
+              create: {
+                address: item.publicProfile.address,
+                ens: item.publicProfile.ens,
+                username: item.publicProfile.username,
+                description: item.publicProfile.description,
+                pfp: item.publicProfile.pfp,
+                cover: item.publicProfile.cover,
+                metoken: item.publicProfile.metoken,
+              },
+            },
+          },
+        });
+      }),
+    );
+
+    // const newUsers = await Promise.all(
+    //   users.map(async (user: any) => {
+    //     const metokensArray = await getOwnerMetoken(user.address);
+    //     const ownerMetokenAddress = metokensArray.length > 0 ? metokensArray[0].id : "";
+    //     const ownerMetokenName = metokensArray.length > 0 ? metokensArray[0].name : "";
+    //     const ownerMetokenSymbol = metokensArray.length > 0 ? metokensArray[0].symbol : "";
+    //     return {
+    //       address: user.address || "",
+    //       email: user.email || "",
+    //       notifications: getNotifications(user),
+    //       oneTimeKey: generateOneTimeKey(),
+    //       isPriorityUser: !!user.isPriorityuser,
+    //       attributes: {},
+    //       priorityRank: !!user.isPriorityuser && !!user.rank ? user.rank : -1,
+    //       gateToken: user.gateToken,
+    //       role: user.isAdmin ? Role.ADMIN : ownerMetokenAddress === "" ? Role.GUEST : Role.USER,
+    //       publicProfile: {
+    //         address: user.address || "",
+    //         ens: user.ens || "",
+    //         username: user.name || "",
+    //         description: user.description || "",
+    //         pfp: getProfileImage(user),
+    //         cover: getCoverImage(user),
+    //         metoken: {
+    //           address: ownerMetokenAddress,
+    //           name: ownerMetokenName,
+    //           symbol: ownerMetokenSymbol,
+    //         },
+    //       },
+    //     };
+    //   }),
+    // );
+
+    return newUsers;
+
+    // return this.prismaService.user.create({
+    //   data: {
+    //     address: "123",
+    //     email: "123",
+    //     role: Role.USER,
+    //     oneTimeKey: generateOneTimeKey(),
+    //   },
+    // });
+
+    // return await this.prismaService.$transaction(
+    //   newUsers.map((user: any) => {
+    //     return this.prismaService.user.create({
+    //       data: {
+    //         address: user.address,
+    //         email: user.email,
+    //         notifications: user.notifications,
+    //         oneTimeKey: user.oneTimeKey,
+    //         role: user.role,
+    //         isPriorityUser: user.isPriorityuser,
+    //         priorityRank: user.priorityRank,
+    //         publicProfile: {
+    //           create: {
+    //             address: user.publicProfile.address,
+    //             ens: user.publicProfile.ens,
+    //             username: user.publicProfile.username,
+    //             description: user.publicProfile.description,
+    //             pfp: user.publicProfile.pfp,
+    //             cover: user.publicProfile.cover,
+    //             metoken: user.publicProfile.metoken,
+    //           },
+    //         },
+    //       },
+    //     });
+    //   }),
+    // );
   }
 
   @Get("/attribute")
@@ -123,3 +254,58 @@ export class UserController {
     return this.userService.getSettings(address);
   }
 }
+
+//
+//
+//
+//
+// Utilities
+
+export function generateOneTimeKey() {
+  return `${generateRandomString(8)}-${generateRandomString(6)}-${generateRandomString(8)}`;
+}
+
+export function generateRandomString(length = 6) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const getNotifications = (user: any) => {
+  if (!user.notificationPreferences) return [];
+  const arr = Object.entries(user.notificationPreferences)
+    .filter(([key, value]) => value)
+    .map(([key, value]) => key.toUpperCase());
+  return arr;
+};
+
+const getRole = (user: any) => {
+  if (user.isAdmin) return Role.ADMIN;
+  if (getMetokenObject(user)) return Role.USER;
+  return Role.GUEST;
+};
+
+const getProfileImage = (user: any) => {
+  if (user.pfp && user.pfp !== "") return user.pfp;
+  if (user.image?.original?.src && user.image?.original?.src !== "") return user.image.original.src;
+  return "";
+};
+
+const getCoverImage = (user: any) => {
+  if (user.cover && user.cover !== "") return user.cover;
+  if (user.background?.original?.src && user.background?.original?.src !== "")
+    return user.background.original.src;
+  return "";
+};
+
+const getMetokenObject = (user: any) => {
+  return {
+    address: user.metoken?.address || "",
+    name: user.metoken?.name || "",
+    symbol: user.metoken?.symbol || "",
+  };
+};
